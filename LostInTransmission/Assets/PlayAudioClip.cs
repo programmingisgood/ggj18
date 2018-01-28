31b013517df32d95c9d588dcaa2ccb4f394193af
Tyler Jones <tyler@lamestargames.com>
2018-01-28 14:26:38 -0800

    Added sound files

---
+++ LostInTransmission/Assets/PlayAudioClip.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudioClip : MonoBehaviour 
+{
+	private AudioSource audio;
+
+	void Start () 
+	{
+		audio = this.GetComponent<AudioSource>();
+	}
+	
+	public void playAudio()
+	{
+		audio.PlayOneShot(audio.clip);
+	}
+}
